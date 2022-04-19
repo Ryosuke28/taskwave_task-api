f9fd653728030e93251e5f26d79849e3bcf1cd00
Ryosuke28 <castanets28@gmail.com>
2022-04-19 10:57:29 +0900

    add gem

--- config/application.rb
+++ config/application.rb
@@ -36,5 +36,19 @@ module TaskApi
     # Middleware like session, flash, cookies can be added back manually.
     # Skip views, helpers and assets when generating a new resource.
     config.api_only = true
+
+    # i18n
+    # 言語ファイルを階層ごとに設定するための記述
+    config.i18n.load_path += Dir[Rails.root.join('config', 'locales', '**', '*.{rb,yml}').to_s]
+
+    # アプリケーションが対応している言語のホワイトリスト(ja = 日本語, en = 英語)
+    config.i18n.available_locales = [:ja, :en]
+
+    # 上記の対応言語以外の言語が指定された場合、エラーとするかの設定
+    config.i18n.enforce_available_locales = true
+
+    # デフォルトの言語設定
+    config.i18n.default_locale = :ja
+  end
   end
 end
