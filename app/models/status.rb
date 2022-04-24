d5f00a5f558bef1e7538d72f03ba8429737fd3d9
Ryosuke28 <58348028+Ryosuke28@users.noreply.github.com>
2022-04-24 16:26:58 +0900

    Revert "グループ作成API作成"

--- app/models/status.rb
+++
@@ -1,3 +0,0 @@
-class Status < ApplicationRecord
-  validates :name, presence: true
-end
--- spec/factories/groups.rb
+++
@@ -1,6 +0,0 @@
-FactoryBot.define do
-  factory :group do
-    sequence(:name) { |n| "name#{n}" }
-    sequence(:team_id) { |n| n }
-  end
-end
